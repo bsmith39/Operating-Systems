7b74c1e18560a04013289288043bba7f05f30fe0
bsmith39 <bsmith39@hawk.iit.edu>
2019-01-19 17:14:05 -0600

    Finished my-cat, passes all tests

--- my-cat.c
+++ my-cat.c
@@ -6,11 +6,14 @@ int main (int argc, char ** argv) {
 	int i = 1;
 	while (argv[i] != NULL){
 		int check = 0;
+		
 		FILE *fp = fopen(argv[i], "r");
+		if (fp == NULL){
+			printf("my-cat: cannot open file\n");
+			exit(1);
+		}
 		char buffer[500];
-		
 		char *g = fgets(buffer, 500, fp);
-		
 		while (g != NULL){
 			printf("%s", buffer);
 			g = fgets(buffer, 500, fp);
@@ -20,6 +23,7 @@ int main (int argc, char ** argv) {
 		if (check == 0){
 			exit(0);
 		}
+		
 		i = i + 1;
 	}
 	return 0;
