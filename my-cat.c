d5b884448b1f43c000db7457fc97810dfe016bbc
bsmith39 <bsmith39@hawk.iit.edu>
2019-01-19 14:47:14 -0600

    Not finished, saving first attempt at my cat

--- my-cat.c
+++ my-cat.c
@@ -3,5 +3,21 @@
 
 
 int main (int argc, char ** argv) {
-    return 0;
+	int i = 1;
+	while (argv[i] != NULL){
+		FILE *fp = fopen(argv[i], "r");
+		char buffer[255];
+		char *g = fgets(buffer, 255, fp);
+		if (g != NULL){
+			printf("%s", buffer);
+			fclose(fp);
+		}else{
+			if (i == 1){
+				exit(0);
+			}
+			exit(1);
+		}
+		i = i+1;
+	}
+	return 0;
 }
